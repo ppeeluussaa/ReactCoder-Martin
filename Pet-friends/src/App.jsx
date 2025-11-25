1b86364e688dd73819d8c03b2cd6d5bacbe56903
Eugenio Martin <eugenioymarcelo2006@gmail.com>
2025-11-24 21:12:05 -0300

    Entrega proyecto React - funcionando

---
+++ Pet-friends/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Mi Proyecto React</h1>
+      <p>Bienvenido a la interfaz principal de la aplicación.</p>
+    </div>
+  );
+}
+
+export default App;
